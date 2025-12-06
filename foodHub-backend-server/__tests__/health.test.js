8af3b207f39f72359572ca42b6430ef129206c05
khang <khangau98@example.com>
2025-12-06 18:56:13 +0700

    test login API

--- foodHub-backend-server/__tests__/health.test.js
+++ foodHub-backend-server/__tests__/health.test.js
@@ -3,10 +3,17 @@
 const request = require('supertest');
 const app = require('../app.js');   // nếu file chính là index.js hoặc server.js thì đổi thành ../index.js
 
-describe('GET /greet', () => {
+describe('API FoodHub – Test thật nhưng vẫn không cần DB thật', () => {
   it('should respond with "Hello, World!"', async () => {
     const res = await request(app).get('/greet');
     expect(res.statusCode).toEqual(200);
     expect(res.text).toBe('Hello, World!');
+  });
+  it('POST /auth/login sai mật khẩu → trả về 401', async () => {
+    const res = await request(app)
+      .post('/auth/login')
+      .send({ email: 'c1@gmail.com', password: '12345678' });
+    expect(res.status).toBe(401);
+    expect(res.body.message).toContain('Logged-in successfully');
   });
 });
